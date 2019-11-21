74c54698a2d0e1fabc89fe54eddca51b43b1b69a
eloylp <eloylpdev@gmail.com>
2019-11-21 22:27:55 +0100

    Add config checking to main

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
+import (
+	"log"
+)
+
 func main() {
 
 	cfg := Config()
+	if err := CheckConfig(cfg); err != nil {
+		log.Fatal(err)
+	}
 	b := RandomBoard(cfg.boardX, cfg.boardY)
 	g := NewGame(b, cfg.generations, cfg.intervalSecs)
 	g.Run()
