60cb0af9868cd85f1c2115f36c0cc4a580a62de0
eloylp <eloylpdev@gmail.com>
2019-11-22 23:42:46 +0100

    Add Go releaser and version to binary

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 )
+
+var Version string
 
 func main() {
 
@@ -10,6 +13,8 @@ func main() {
 	if err := CheckConfig(cfg); err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Conway`s game of life")
+	fmt.Println(Version)
 	b := RandomBoard(cfg.boardX, cfg.boardY)
 	g := NewGame(b, cfg.generations, cfg.intervalSecs)
 	g.Run()
